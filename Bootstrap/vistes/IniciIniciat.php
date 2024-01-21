dbcc57157f075db0b4bfb32b48ec953028264286
AymanSbayy <a.sbay@sapalomera.cat>
2024-01-21 18:44:05 +0100

    Projectefinalitzat

---
+++ Bootstrap/vistes/IniciIniciat.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Inicio</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="../estils/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <nav class="navbar navbar-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><i class="fas fa-home" style="color: white;"></i>  <b>Inicio</b></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <hr>
+            <div style="margin-right: 10px;" class="divv2">
+              <img class="imgg" src="../imgs/paisa.jpg" alt="User">
+              <div style="display: flex; flex-direction: column; margin-left: 17px; flex-grow: 1; color: #9584F8;">
+                <p style="color:#fff; font-size: 24px;">Ayman Sbay Zekkari</p>
+                <p style="color:#fff; font-size: 18px;">ayman.zekkari@gmail.com</p>
+                <p style="color:#fff; font-size: 18px;">C/ Josep Carner, 5 , 3/2</p>
+              </div>
+            </div>
+            <hr>
+
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a style="color:#fff;" class="nav-link" href="../index.php">Cerrar sesión</a>
+              </li>
+              <li class="nav-item">
+                <a style="color:#fff;" class="nav-link" href="#">Cambiar constraseña</a>
+              </li>
+              <hr>
+              <li class="nav-item">
+                <a style="color:#fff;" class="nav-link" href="login.php">Darse de baja</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <br>
+  <br>
+  <div class="container my-4">
+    <div class="d-flex justify-content-between align-items-center">
+      <h3>Articles</h3>
+      <div>
+        <select name="articles" id="articles" class="form-select d-inline-block"
+          style="width: auto; margin-right: 10px;">
+          <option value="3">3</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </select>
+        <input type="search" class="form-control d-inline-block" placeholder="Buscar articles"
+          style="width: auto; margin-right: 10px;">
+        <a class="link" href="inserir.php" title="Inserir article">
+          <img src="../imgs/insert.png" alt="Añadir" style="margin-bottom:5px; width: 26px; height: 26px;">
+        </a>
+      </div>
+    </div>
+    <hr>
+  </div>
+  <br>
+  <br>
+  <div class="container my-4 divv">
+    <img class="img4" src="../imgs/paisa.jpg" alt="...">
+    <div style="align-items: column; padding-left:47px; ">
+      <div style="display:flex; flex-direction: row; justify-content: space-between;">
+        <h4>Pronombres personales</h4>
+        <div>
+          <a class="link" href="modificar.php" title="Editar article">
+            <img class="imggg" src="https://cdn.icon-icons.com/icons2/931/PNG/512/edit_modify_icon-icons.com_72390.png"
+              alt="Editar" />
+          </a>
+          <a href="#" class="link" title="Eliminar article">
+            <img class="img3" src="https://cdn-icons-png.flaticon.com/512/70/70287.png" alt="Eliminar" />
+          </a>
+        </div>
+      </div>
+      <div style="display: flex; flex-direction: column; flex-grow: 1;">
+        <hr style="width: 101%; margin-top: 0px;">
+        <p>Existen diferentes clases de pronombres, en este caso vamos a estudiar los “personales” que son aquellas
+          palabras que reemplazan a los sustantivos que se refieren a personas, es decir, son una manera de referirse a
+          ellos sin nombrarlos, sin aclarar el sustantivo exacto. </p>
+      </div>
+    </div>
+  </div>
+  <div class="container my-4 divv">
+    <img class="img4" src="../imgs/paisa.jpg" alt="...">
+    <div style="align-items: column; padding-left:47px; ">
+      <div style="display:flex; flex-direction: row; justify-content: space-between;">
+        <h4>Pronombres personales</h4>
+        <div>
+          <a class="link" href="modificar.php" title="Editar article">
+            <img class="imggg" src="https://cdn.icon-icons.com/icons2/931/PNG/512/edit_modify_icon-icons.com_72390.png"
+              alt="Editar" />
+          </a>
+          <a href="#" class="link" title="Eliminar article">
+            <img class="img3" src="https://cdn-icons-png.flaticon.com/512/70/70287.png" alt="Eliminar" />
+          </a>
+        </div>
+      </div>
+      <div style="display: flex; flex-direction: column; flex-grow: 1;">
+        <hr style="width: 101%; margin-top: 0px;">
+        <p>Existen diferentes clases de pronombres, en este caso vamos a estudiar los “personales” que son aquellas
+          palabras que reemplazan a los sustantivos que se refieren a personas, es decir, son una manera de referirse a
+          ellos sin nombrarlos, sin aclarar el sustantivo exacto. </p>
+      </div>
+    </div>
+  </div>
+  <div class="container my-4 divv">
+    <img class="img4" src="../imgs/paisa.jpg" alt="...">
+    <div style="align-items: column; padding-left:47px; ">
+      <div style="display:flex; flex-direction: row; justify-content: space-between;">
+        <h4>Pronombres personales</h4>
+        <div>
+          <a class="link" href="modificar.php" title="Editar article">
+            <img class="imggg" src="https://cdn.icon-icons.com/icons2/931/PNG/512/edit_modify_icon-icons.com_72390.png"
+              alt="Editar" />
+          </a>
+          <a href="#" class="link" title="Eliminar article">
+            <img class="img3" src="https://cdn-icons-png.flaticon.com/512/70/70287.png" alt="Eliminar" />
+          </a>
+        </div>
+      </div>
+      <div style="display: flex; flex-direction: column; flex-grow: 1;">
+        <hr style="width: 101%; margin-top: 0px;">
+        <p>Existen diferentes clases de pronombres, en este caso vamos a estudiar los “personales” que son aquellas
+          palabras que reemplazan a los sustantivos que se refieren a personas, es decir, son una manera de referirse a
+          ellos sin nombrarlos, sin aclarar el sustantivo exacto. </p>
+      </div>
+    </div>
+  </div>
+  <br>
+
+  <div class="container paginacion">
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">Següent</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <footer class="footer">
+    <p class="footer-text">&copy; 2024 Ayman Sbay Zekkari. Todos los derechos reservados.</p>
+    <p class="footer-text">Síguenos en nuestras redes sociales.</p>
+  </footer>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Bootstrap/vistes/inserir.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Insertar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../estils/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="IniciIniciat.php"><i class="fas fa-home" style="color: white;"></i>
+                        <b>Inicio</b></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <hr>
+                        <div style="margin-right: 10px;" class="divv2">
+                            <img class="imgg" src="../imgs/paisa.jpg" alt="User">
+                            <div
+                                style="display: flex; flex-direction: column; margin-left: 17px; flex-grow: 1; color: #9584F8;">
+                                <p style="color:#fff; font-size: 24px;">Ayman Sbay Zekkari</p>
+                                <p style="color:#fff; font-size: 18px;">ayman.zekkari@gmail.com</p>
+                                <p style="color:#fff; font-size: 18px;">C/ Josep Carner, 5 , 3/2</p>
+                            </div>
+                        </div>
+                        <hr>
+
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a style="color:#fff;" class="nav-link" href="../index.php">Cerrar sesión</a>
+                            </li>
+                            <li class="nav-item">
+                                <a style="color:#fff;" class="nav-link" href="#">Cambiar constraseña</a>
+                            </li>
+                            <hr>
+                            <li class="nav-item">
+                                <a style="color:#fff;" class="nav-link" href="login.php">Darse de baja</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+
+    <main class="container mt-5">
+        <div class="card p-4">
+            <h2 class="text-center">Insertar artículo</h2>
+            <form>
+                <div class="mb-3">
+                    <label for="titulo" class="form-label">Título*</label>
+                    <input type="text" class="form-control" id="titulo" required>
+                </div>
+                <div class="mb-3">
+                    <label for="autor" class="form-label">Autor*</label>
+                    <input type="text" class="form-control" id="autor" required>
+                </div>
+                <div class="mb-3">
+                    <label for="articulo" class="form-label">Artículo*</label>
+                    <textarea class="form-control" id="articulo" rows="6" required></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="imagen" class="form-label">Imagen</label>
+                    <input class="form-control" type="file" id="imagen">
+                </div>
+                <button type="submit" class="btn btn-primary">Insertar</button>
+            </form>
+        </div>
+    </main>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <footer class="footer">
+        <p class="footer-text">&copy; 2024 Ayman Sbay Zekkari. Todos los derechos reservados.</p>
+        <p class="footer-text">Síguenos en nuestras redes sociales.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
